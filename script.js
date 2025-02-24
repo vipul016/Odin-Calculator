4b3de9bda1aa77d2831a5267e5590b8851cdd1ab
vipul016 <vipulwaskel934@gmail.com>
2025-02-24 18:30:09 +0530

    simple operations added

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 const inputBox=document.querySelector(".text");
 console.log(inputBox.textContent);
-inputBox.innerHTML=5;
 
 const box=document.querySelectorAll(".box");
-box.forEach((boxes,index)=>{
-    boxes.id=`box-${index+1}`;
-});
-
+let op=[];
+let input='';
 box.forEach((boxes,index)=>{
     boxes.addEventListener("click",(e)=>{
+        let val=e.target.textContent;
+        const check=['='];
         const arr=['1','2','3','4','5','6','7','8','9','0'];
-        if(arr.includes(e.target.textContent)){
-            inputBox.textContent=e.target.textContent;
+        const clear=['AC'];
+        if(clear.includes(val)){
+            input='';
+            op=[];
+            inputBox.textContent=0;
         }
-        
+        if(arr.includes(val)){
+            input+=val;
+            inputBox.textContent=input;   
+        }
+        else if(!arr.includes(val) && !check.includes(val) && !clear.includes(val)){
+           if(input!==''){
+            op.push(parseInt(input));
+            op.push(val);
+            input='';
+           }
+        }else if(check.includes(val)){
+            if(input!==''){
+                op.push(parseInt(input));
+                console.log(calculate(op));
+                inputBox.textContent=calculate(op);
+                input=calculate(op);
+                op.push(calculate(op));
+            }
+        }
     });
 });
+
+const calculate=(arr)=>{
+    let result=arr[0];
+    for(let i=1;i<arr.length;i+=2){
+        let num=arr[i+1];
+        if(arr[i]==='+'){
+            result+=num;
+        }else if(arr[i]=='-'){
+            result-=num;
+        }else if(arr[i]=='x'){
+            result*=num;
+        }else if(arr[i]=='/'){
+            result/=num;
+        }
+    }
+    return result;
+}
